dd992a8a97c75cfb730555d46e4ef44991ace4eb
zeeshanAhmed2798 <154904813+zeeshan-7BS@users.noreply.github.com>
2025-11-03 01:22:05 +0500

    update embedding

--- services/embedding.py
+++ services/embedding.py
@@ -1,5 +1,5 @@
 from typing import List
-from langchain.schema import Document
+from langchain_core.documents import Document
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from config import settings
 
